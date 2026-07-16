46622c5ddd1dff349ad1ef75244bd6c0d8ae8060
mrgin0 <97049921+mrgin0@users.noreply.github.com>
2026-07-16 10:13:59 +0800

    Add Firebase configuration to config.js
    Added Firebase configuration for the project.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,10 @@
-// Konfigurasi koneksi Supabase (URL + anon key)
-
-const SB='https://prybtaghmbajrahijrjt.supabase.co/rest/v1';
-const AK='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByeWJ0YWdobWJhanJhaGlqcmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTc0NDMsImV4cCI6MjA5NzIzMzQ0M30.kALEkLvw4a4aQYEgnYvdajUP4YG1yvjKfhPqBaakAnM';
-const H={'Content-Type':'application/json','apikey':AK,'Authorization':'Bearer '+AK,'Prefer':'return=representation'};
+// Konfigurasi koneksi Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD0LuYrwJ3pZ2H7l52N472uD0aQpHLi_40",
+  authDomain: "laporan-keuangan-9b7b0.firebaseapp.com",
+  projectId: "laporan-keuangan-9b7b0",
+  storageBucket: "laporan-keuangan-9b7b0.firebasestorage.app",
+  messagingSenderId: "248122424099",
+  appId: "248122424099"
+};
+firebase.initializeApp(firebaseConfig);
